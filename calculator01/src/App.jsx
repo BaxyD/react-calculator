0e6a89e227fac399bac2385b35c2bab8065d6e0c
BaxyD <112361061+BaxyD@users.noreply.github.com>
2023-09-10 12:54:33 -0500

    update App.jsx

--- calculator01/src/App.jsx
+++ calculator01/src/App.jsx
@@ -4,47 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-function Button({ label, onClick }) {
-    return <button onClick={() => onClick(label)}>{label}</button>;
+  const [input, setInput] = useState('');
+  const[result, setResult] = useState('');
+
+  function handleButtonClick(label) {
+    if (label === 'c') {
+      // Clear the input and result
+      setInput('');
+      setResult('');
+    } else if (label === '=') {
+      try {
+        // Evaluate the math expression and set the result
+        setResult(eval(input));
+      } catch (error) {
+        // handle invalid expressions
+        setResult('Error');
+      }
+    } else {
+      // update the input with the clicked button label
+      setInput((prevInput) => prevInput + label);
+    }
   }
 
-function Display({ input, result }) {
-  return (
-    <div className='calculator-display'>
-      <div className="input">{input}</div>
-      <div className='result'>{result}</div>
-    </div>
-  );
-}   
+  const buttons = [
+    'c', '%' , '/', '', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '', '='
+  ];
 
   return (
     <>
     <div className='calculator'>
-      <div className='calculator-display'></div>
-      <button className='btn-c btn' >c</button>
-      <button className='btn-% btn' >%</button>
-      <button className='btn-/ btn'>/</button>
-      <button className='btn'></button>
-      <button className='btn-7 btn'>7</button>
-      <button className='btn-8 btn'>8</button>
-      <button className='btn-9 btn'>9</button>
-      <button className='btn-x btn'>x</button>
-      <button className='btn-4 btn'>4</button>
-      <button className='btn-5 btn'>5</button>
-      <button className='btn-6 btn'>6</button>
-      <button className='btn-- btn'>-</button>
-      <button className='btn-1 btn'>1</button>
-      <button className='btn-2 btn'>2</button>
-      <button className='btn-3 btn'>3</button>
-      <button className='btn-+ btn'>+</button>
-      <button className='btn-0 btn'>0</button>
-      <button className='btn-. btn'>.</button>
-      <button className='btn'></button>
-      <button className='btn-= btn'>=</button>
+      <div className='calculator-display'>
+        <div className='input'>{input}</div>
+        <div className='result'>{result}</div>
+      </div>
+      {buttons.map((label, index) => (
+        <button className={`btn-${label} btn`} key={index} onClick={() => handleButtonClick(label)}>
+          {label}
+        </button>
+      ))}
     </div>
     </>
-  )
+  );
 }
 
 
